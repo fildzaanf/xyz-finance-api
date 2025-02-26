8da2ba7fe71be735dc0be19de264110ea09fa639
Hanisah Fildza Annafisah <hanisahfildza@gmail.com>
2025-02-26 13:32:04 +0700

    feature-user: add internal usecase

--- internal/user/usecase/query.go
+++ internal/user/usecase/query.go
@@ -1,1 +1,33 @@
 package usecase
+
+import (
+	"errors"
+	"xyz-finance-api/pkg/constant"
+	"xyz-finance-api/internal/user/domain"
+	"xyz-finance-api/internal/user/repository"
+)
+
+type userQueryUsecase struct {
+	userCommandRepository repository.UserCommandRepositoryInterface
+	userQueryRepository   repository.UserQueryRepositoryInterface
+}
+
+func NewUserQueryUsecase(ucr repository.UserCommandRepositoryInterface, uqr repository.UserQueryRepositoryInterface) UserQueryUsecaseInterface {
+	return &userQueryUsecase{
+		userCommandRepository: ucr,
+		userQueryRepository:   uqr,
+	}
+}
+
+func (uqs *userQueryUsecase) GetUserByID(id string) (domain.User, error) {
+	if id == "" {
+		return domain.User{}, errors.New(constant.ERROR_ID_INVALID)
+	}
+
+	userDomain, errGetID := uqs.userQueryRepository.GetUserByID(id)
+	if errGetID != nil {
+		return domain.User{}, errors.New(constant.ERROR_DATA_EMPTY)
+	}
+
+	return userDomain, nil
+}
