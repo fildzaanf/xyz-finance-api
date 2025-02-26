eeedc6ecc6a847707277596f68c9c5a57c0a0b9c
Hanisah Fildza Annafisah <hanisahfildza@gmail.com>
2025-02-26 13:32:30 +0700

    feature-user: add internal handler

--- internal/user/handler/interface.go
+++ internal/user/handler/interface.go
@@ -1,2 +1,12 @@
 package handler
 
+import "github.com/labstack/echo/v4"
+
+type UserHandlerInterface interface {
+	// Query
+	GetUserByID(c echo.Context) error
+
+	// Command
+	RegisterUser(c echo.Context) error
+	LoginUser(c echo.Context) error
+}
